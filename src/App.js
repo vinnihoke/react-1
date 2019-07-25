325cc6d1f283c278dd9a469bb62b0180997fb04c
Vinni Hoke <vinni.hoke@gmail.com>
2019-07-24 20:29:12 -0700

    Completed Partial Stretch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 
   const [ homeTeam, homeScore ] = useState(0);
   const [ awayTeam, awayScore ] = useState(0);
-  const [counter, setCounter] = useState(0);
+  const [ counter, setCounter ] = useState(0);
 
   const handleHomeTD = () => homeScore(homeTeam + 7)
   const handleHomeFG = () => homeScore(homeTeam + 3)
@@ -16,6 +16,16 @@ function App() {
   const handleAwayTD = () => awayScore(awayTeam + 7)
   const handleAwayFG = () => awayScore(awayTeam + 3)
 
+  const [ home, setHome ] = useState('');
+  const [ away, setAway ] = useState('');
+
+  const handleReset = () => {
+    setHome('');
+    setAway('');
+    homeScore(0);
+    awayScore(0);
+    setCounter(0);
+  }
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -33,7 +43,7 @@ function App() {
       <section className="scoreboard">
         <div className="topRow">
           <div className="home">
-            <h2 className="home__name">Lions</h2>
+            <h2 className="home__name">{home}</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
@@ -41,11 +51,11 @@ function App() {
           </div>
           <div className="timer" >{counter}</div>
           <div className="away">
-            <h2 className="away__name">Tigers</h2>
+            <h2 className="away__name">{away}</h2>
             <div className="away__score">{awayTeam}</div>
           </div>
         </div>
-        <BottomRow />
+        <BottomRow prop={}/>
       </section>
       <section className="buttons">
         <div className="homeButtons">
@@ -56,6 +66,15 @@ function App() {
         <div className="awayButtons">
           <button className="awayButtons__touchdown" onClick={handleAwayTD}>Away Touchdown</button>
           <button className="awayButtons__fieldGoal" onClick={handleAwayFG}>Away Field Goal</button>
+        </div>
+        <div className="gameButtons">
+          <button className="gameButtons__reset" onClick={handleReset}>Reset</button>
+        </div>
+        <div className="players">
+          <label>Set Home Team</label>
+          <input type="text" placeholder="Set Home Team" onChange={e => setHome(e.target.value)}/>
+          <label>Set Away Team</label>
+          <input type="text" placeholder="Set Away Team" onChange={e => setAway(e.target.value)}/>
         </div>
       </section>
     </div>
